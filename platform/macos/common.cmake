05e73b090fdc13cf4c624f262ae7908fae2377b1
Sharvil Shah <sharvil@sharvilshah.com>
2021-06-07 20:17:19 +0530

    [wip] towards macos app bundle

--- platform/macos/common.cmake
+++ platform/macos/common.cmake
@@ -9,14 +9,46 @@
 
 set(OSQUERY_PACKAGE_RELEASE "1.macos")
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}_${OSQUERY_PACKAGE_RELEASE}_${CMAKE_SYSTEM_PROCESSOR}")
+#set(CPACK_PACKAGING_INSTALL_PREFIX "opt")
+#set(CMAKE_INSTALL_PREFIX "/opt")
 set(CPACK_SET_DESTDIR ON)
+
 
 install(
   FILES
-    "${OSQUERY_DATA_PATH}/usr/local/bin/osqueryd"
-    "${OSQUERY_DATA_PATH}/usr/local/bin/osqueryi"
-    "${OSQUERY_DATA_PATH}/usr/local/bin/osqueryctl"
+    "${OSQUERY_DATA_PATH}/opt/bin/osqueryd"
 
+  DESTINATION
+    "/opt/osquery.app/Contents/MacOS"
+
+  COMPONENT
+    osquery
+
+  PERMISSIONS
+    OWNER_READ OWNER_WRITE OWNER_EXECUTE
+    GROUP_READ             GROUP_EXECUTE
+    WORLD_READ             WORLD_EXECUTE 
+)
+
+install(
+  FILES
+    "${OSQUERY_DATA_PATH}/control/Info.plist"
+    "${OSQUERY_DATA_PATH}/control/PkgInfo"
+    "${OSQUERY_DATA_PATH}/control/embedded.provisionprofile"
+
+  DESTINATION
+    "/opt/osquery.app/Contents"
+
+  COMPONENT
+    osquery
+)
+
+install(
+  FILES
+    "${OSQUERY_DATA_PATH}/opt/bin/osqueryi"
+    "${OSQUERY_DATA_PATH}/opt/bin/osqueryctl"
+
+  # /usr/local is the default packaging prefix
   DESTINATION
     "bin"
 
