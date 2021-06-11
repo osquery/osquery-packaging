4ccc818aabcf6c3df6b720d5f59e9eee42716726
Teddy Reed <teddy@casualhacking.io>
2021-06-11 16:55:35 -0400

    windows: Select appropriate Program Files directory

--- platform/windows/common.cmake
+++ platform/windows/common.cmake
@@ -10,6 +10,12 @@
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 set(OSQUERY_BITNESS "" CACHE STRING "osquery build bitness (32 or 64)")
 
+if ("${OSQUERY_BITNESS}" STREQUAL "32")
+  set(PROGRAM_FILES_DIR "Program Files (x86)")
+else()
+  set(PROGRAM_FILES_DIR "Program Files")
+endif()
+
 set(directory_name_list
   "certs"
   "log"
@@ -26,7 +32,7 @@ set(file_name_list
 
 foreach(directory_name ${directory_name_list})
   install(
-    DIRECTORY "${OSQUERY_DATA_PATH}/Program Files/osquery/${directory_name}"
+    DIRECTORY "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${directory_name}"
     DESTINATION "."
     COMPONENT osquery
   )
@@ -34,14 +40,14 @@ endforeach()
 
 foreach(file_name ${file_name_list})
   install(
-    FILES "${OSQUERY_DATA_PATH}/Program Files/osquery/${file_name}"
+    FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/${file_name}"
     DESTINATION "."
     COMPONENT osquery
   )
 endforeach()
 
 install(
-  FILES "${OSQUERY_DATA_PATH}/Program Files/osquery/osqueryd/osqueryd.exe"
+  FILES "${OSQUERY_DATA_PATH}/${PROGRAM_FILES_DIR}/osquery/osqueryd/osqueryd.exe"
   DESTINATION "osqueryd"
   COMPONENT osquery
 )
